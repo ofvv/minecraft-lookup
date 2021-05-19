2ddf8f946d02ad10706fa0764d0eaa121d95a24a
Niki <contact@zirobot.xyz>
2021-05-19 21:53:11 +0300

    added namemcfriends()

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,10 @@
 const mcapi = require("minecraft-lookup");
 
-mcapi.api.skin("ZiroCore").then(data => console.log(data));
-mcapi.api.user("ZiroCore").then(data => console.log(data));
-mcapi.api.server("hypixel.net").then(data => console.log(data));
-mcapi.api.head("ZiroCore").then(data => console.log(data));
-mcapi.api.status().then(data => console.log(data));
+mcapi.skin("ZiroCore").then(data => console.log(data));
+mcapi.user("ZiroCore").then(data => console.log(data));
+mcapi.server("hypixel.net").then(data => console.log(data));
+mcapi.head("ZiroCore").then(data => console.log(data));
+mcapi.status().then(data => console.log(data));
+mcapi.namemcfriends("adf01e23-43c8-44e7-b039-a0981da5a093").then(data => console.log(data))
 // works pog
 // added status
