5d545af38649739704654ded22811478431d1c28
Niki <contact@zirobot.xyz>
2021-05-21 14:24:42 +0300

    changed require()

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,11 @@
-const mcapi = require("../index");
+const mcapi = require(`${process.cwd()}/library/index.js`);
 
 mcapi.skin("ZiroCore").then(data => console.log(data));
 mcapi.user("ZiroCore").then(data => console.log(data));
 mcapi.server("hypixel.net").then(data => console.log(data));
 mcapi.head("ZiroCore").then(data => console.log(data));
-mcapi.status().then(data => console.log(data));
 mcapi.namemcfriends("adf01e23-43c8-44e7-b039-a0981da5a093").then(data => console.log(data))
-console.log(mcapi.ofCape("cessabit"));
+mcapi.nameHistory("username", "ZiroCore").then(data => console.log(data))
+mcapi.status().then(data => console.log(data));
+mcapi.ofCape("Notch").then(data => console.log(data));
 // works pog
-
